068d66c56d3c2299dc40ea60490ce99a1c1e2598
Hasankhan170 <hasankhanali0316@gmail.com>
2024-02-22 06:50:45 -0800

    index file created

---
+++ app.js
@@ -0,0 +1,82 @@
+
+// CHAPTER 1 ALERTS STARTED
+
+
+// question.1 
+// alert("");
+// done 
+
+// question.2
+
+// alert("script");
+// done 
+
+// question.3
+// alert("Welcome to JS Land\nHappy coding!" );
+// not done 
+
+// question.4
+// alert("welcome to JS Land...");
+// alert("Happy Coding!");
+// done 
+
+// question.5
+// alert("Hello... I can run JS through my web browser's consloe")
+// DONE 
+
+
+// question.7
+// C: BODY{INSIDE YOUR PAGE'S HTML} 
+// DONE 
+
+
+// CHAPTER 1 ALERTS END 
+
+
+// CHAPTER 2 VARIABLS FOR STRING STARTED
+ 
+
+// question 1 and 2 
+// var userName = "my name is hasan khan";
+// console.log(userName);
+// done 
+
+// question 3
+// var massage ="Hello World";
+// alert(massage);
+// done 
+
+// question 4 
+// var userName = "hasan khan";
+// var age = "19";
+// var education = "Certified Mobile Application Development";
+
+// alert(userName);
+// alert(age);
+// alert(education);
+// done 
+
+// question 5 
+// var food ="PIZAA\nPIZZ\nPIZ\nPI\nP\n";
+// alert(food);
+// done 
+
+// question 6
+// var email = "My email adsress is Hasankhan1234@email.com";
+// alert(email);
+// done 
+
+
+// CHAPTER 3 VARIABLS FOR NUMBER STARTED
+// question 1
+// var age ="i am 19 year old";
+// alert(age);
+// done 
+
+// question 3
+// var birth ="My Birth Year is 2004"
+// var Year = "Data type of my declared variable is number"
+// document.getElementById("main").innerHTML = birth;
+// document.getElementById("main-1").innerHTML = Year;
+// DONE 
+
